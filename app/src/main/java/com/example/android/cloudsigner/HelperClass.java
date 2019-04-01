8e132139ee02e0af5d57b476d31433806e528f79
Urian Paul <urianpaul29@gmail.com>
2019-04-01 11:52:31 +0300

    News

--- app/src/main/java/com/example/android/cloudsigner/HelperClass.java
+++ app/src/main/java/com/example/android/cloudsigner/HelperClass.java
@@ -1,14 +1,20 @@
 package com.example.android.cloudsigner;
 
 import android.app.AlertDialog;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Toast;
 
 import static android.content.Context.CONNECTIVITY_SERVICE;
@@ -30,6 +36,158 @@ public class HelperClass {
         }
         return false;
     }
+
+    public boolean saveValue(Context context, String type, String value) {
+        try {
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(type, value);
+            editor.apply();
+            return true;
+        } catch (Exception e) {
+            Log.d("Error: ", e.getMessage());
+            return false;
+        }
+    }
+
+    public String getValue(Context context, String type) {
+        try {
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+            String value = sharedPreferences.getString(type, null);
+
+            if (value != null) {
+                return value;
+            } else {
+                return "Not found!";
+            }
+        } catch (Exception e) {
+            Log.d("Error: ", e.getMessage());
+            return null;
+        }
+    }
+
+    public String getUriRealPathAboveKitKat(Context context, Uri uri) {
+        String path = "";
+
+        if (context != null && uri != null) {
+            if (isContentUri(uri)) {
+                ContentResolver contentResolver = context.getContentResolver();
+                path = getRealPath(contentResolver, uri, null);
+            } else if (isFileUri(uri)) {
+                path = uri.getPath();
+            } else {
+                if (isDocumentUri(context, uri)) {
+                    String docId = DocumentsContract.getDocumentId(uri);
+                    String uriAuthority = uri.getAuthority();
+
+                    if (isMediaDoc(uriAuthority)) {
+                        Log.d("Doc: ", "Media");
+                    } else if (isDownloadDoc(uriAuthority)) {
+                        Log.d("Doc: ", "Download");
+                    } else if (isExternalStoreDoc(uriAuthority)) {
+                        Log.d("Doc: ", "External");
+                    }
+                }
+
+            }
+
+        }
+
+        return path;
+    }
+
+    private String getRealPath(ContentResolver contentResolver, Uri uri, String whereClause) {
+        String ret = "";
+        Cursor cursor = contentResolver.query(uri, null, whereClause, null, null);
+
+        if (cursor != null) {
+            boolean moveToFirst = cursor.moveToFirst();
+            if (moveToFirst) {
+                String columnName = MediaStore.Images.Media.DATA;
+                String col = MediaStore.Files.FileColumns.DATA;
+
+                if (uri == MediaStore.Images.Media.EXTERNAL_CONTENT_URI) {
+                    columnName = MediaStore.Images.Media.DATA;
+                } else if (uri == MediaStore.Audio.Media.EXTERNAL_CONTENT_URI) {
+                    columnName = MediaStore.Audio.Media.DATA;
+                } else if (uri == MediaStore.Video.Media.EXTERNAL_CONTENT_URI) {
+                    columnName = MediaStore.Video.Media.DATA;
+                }
+                int imageColumnIndex = cursor.getColumnIndex(columnName);
+                ret = cursor.getString(imageColumnIndex);
+            }
+        }
+
+        return ret;
+    }
+
+
+    private boolean isAboveKitKat() {
+        boolean ret = false;
+        ret = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+        return ret;
+    }
+
+    private boolean isFileUri(Uri uri) {
+        boolean ret = false;
+        if (uri != null) {
+            String uriSchema = uri.getScheme();
+            if ("file".equalsIgnoreCase(uriSchema)) {
+                ret = true;
+            }
+        }
+        return ret;
+    }
+
+    private boolean isDocumentUri(Context ctx, Uri uri) {
+        boolean ret = false;
+        if (ctx != null && uri != null) {
+            ret = DocumentsContract.isDocumentUri(ctx, uri);
+        }
+        return ret;
+    }
+
+    private boolean isContentUri(Uri uri) {
+        boolean ret = false;
+        if (uri != null) {
+            String uriSchema = uri.getScheme();
+            if ("content".equalsIgnoreCase(uriSchema)) {
+                ret = true;
+            }
+        }
+        return ret;
+    }
+
+    private boolean isExternalStoreDoc(String uriAuthority) {
+        boolean ret = false;
+
+        if ("com.android.externalstorage.documents".equals(uriAuthority)) {
+            ret = true;
+        }
+
+        return ret;
+    }
+
+    private boolean isDownloadDoc(String uriAuthority) {
+        boolean ret = false;
+
+        if ("com.android.providers.downloads.documents".equals(uriAuthority)) {
+            ret = true;
+        }
+
+        return ret;
+    }
+
+    private boolean isMediaDoc(String uriAuthority) {
+        boolean ret = false;
+
+        if ("com.android.providers.media.documents".equals(uriAuthority)) {
+            ret = true;
+        }
+
+        return ret;
+    }
+
 
     public void AlertDialogBuilder(String message, final Context context, String title) {
         AlertDialog.Builder newAlert = new AlertDialog.Builder(context);
--- app/src/main/java/com/example/android/cloudsigner/MainActivity.java
+++ app/src/main/java/com/example/android/cloudsigner/MainActivity.java
@@ -7,9 +7,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.graphics.PathUtils;
+import android.support.v4.provider.DocumentFile;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,30 +22,56 @@ import java.io.InputStream;
 import java.net.URI;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String FILE_PATH = "Path";
+    private String authCode = "";
+    private Button signButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SetDateAndTime();
         Intent intent = getIntent();
-
+        if (intent == null) {
+            finish();
+        }
         try {
-            String fileName = intent.getData().getPath();
             Uri uri = intent.getData();
-            String str = intent.getDataString();
+            HelperClass helperClass = new HelperClass(this);
+            String path = helperClass.getUriRealPathAboveKitKat(this, uri);
 
-            InputStream inStream = getContentResolver().openInputStream(uri);
+            if (helperClass.saveValue(this, MainActivity.FILE_PATH, path)) {
+                Log.d("Saved file path to Shared Preferences: ", path);
+            } else {
+                Log.d("Error: ", "Saving path to Shared Preferences failed!.");
+            }
 
-            Log.d("Uri", str);
-            Log.d("Path", fileName);
         } catch (Exception e) {
             Log.d("Exception:", e.getMessage());
         }
+        Button authorize = (Button) findViewById(R.id.button_authorize);
+        signButton = (Button) findViewById(R.id.button_sign);
+        Login(authorize);
+    }
+
+    /* @Override
+     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+         signButton.setVisibility(View.VISIBLE);
+     }*/
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = getIntent();
+
+        
+        if (!authCode.equals("")) {
+            signButton.setVisibility(View.VISIBLE);
+        }
 
 
-        Login();
     }
 
     public void SetDateAndTime() {
@@ -54,41 +82,19 @@ public class MainActivity extends AppCompatActivity {
         date.setText(myDate);
     }
 
-    /* public void Login() {
-         Button btnLogin = (Button) findViewById(R.id.button_login);
-         final EditText emailText = (EditText) findViewById(R.id.edit_phone);
-         final EditText passwordText = (EditText) findViewById(R.id.edit_password);
-         final HelperClass myClass = new HelperClass(this);
-         final Context context = this;
-         btnLogin.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 emailText.setText("");
-                 passwordText.setText("");
-                 if (myClass.InternetConnection() == false) {
-                     myClass.AlertDialogBuilder("You must enable Internet Connection to be able to sign documents!",
-                             context, "Internet Error!");
-                 } else {
-                     startActivity(new Intent(MainActivity.this, OTPpopUp.class));
-                 }
-             }
-         });
-     }*/
-    public void Login() {
-        Button btnLogin = (Button) findViewById(R.id.button_authorize);
-
+    public void Login(Button btn) {
         final String clientSecret = "paulssecret";
-        final String clientID="urian";
+        final String clientID = "urian";
         final String redirectUri = "http://cloud-signer/";
         final String webServiceUrl = "https://msign-test.transsped.ro/csc/v0/";
-        String url = webServiceUrl + "oauth2/authorize?response_type=token&client_id="+clientID+"&client_secret=" + clientSecret + "&redirect_uri=" + redirectUri;
+        String url = webServiceUrl + "oauth2/authorize?response_type=token&client_id=" + clientID + "&client_secret=" + clientSecret + "&redirect_uri=" + redirectUri;
 
         final Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(url));
         final HelperClass myClass = new HelperClass(this);
         final Context context = this;
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (myClass.InternetConnection() == false) {
@@ -100,6 +106,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
 }
